72f5e8732052fe2f9900a2a94da6d406203082b6
Jordan Neufeld <jordan@neufeldtech.com>
2020-04-25 17:28:41 -0500

    Basic request signature working

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/neufeldtech/smsg-go
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/nlopes/slack v0.6.0
+	github.com/prometheus/common v0.9.1
+	go.uber.org/zap v1.14.0
+)
--- main.go
+++ main.go
@@ -2,18 +2,95 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nlopes/slack"
+	"github.com/prometheus/common/log"
 
 	"os"
 )
 
+var (
+	signingSecret, _ = os.LookupEnv("SLACK_SIGNING_SECRET")
+)
+
+type SlashBody struct {
+	Token    string `form:"token" json:"token" binding:"required"`
+	SSLCheck bool   `form:"ssl_check" json:"ssl_check"`
+}
+
+// func SSLCheckInterceptor() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var body SlashBody
+// 		err := c.BindJSON(&body)
+// 		if err != nil {
+// 			// c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "Bad request. expected JSON body"})
+// 			// return
+// 			log.Error(err)
+// 			c.Next()
+// 			return
+// 		}
+// 		if body.SSLCheck {
+// 			c.AbortWithStatusJSON(http.StatusOK, gin.H{"status": "OK"})
+// 			c.Next()
+// 			return
+// 		}
+// 		c.Next()
+// 	}
+// }
+
+func ValidateSignature() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		verifier, err := slack.NewSecretsVerifier(c.Request.Header, signingSecret)
+		if err != nil {
+			log.Errorf("error verifying signature: %v", err)
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"status": "Error verifying secret"})
+			return
+		}
+		c.Request.Body = ioutil.NopCloser(io.TeeReader(c.Request.Body, &verifier))
+		_, err = slack.SlashCommandParse(c.Request)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		if err = verifier.Ensure(); err != nil {
+			log.Error(err)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "Signature not valid"})
+			return
+		}
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.Use(gin.Logger())
+	// r.Use(SSLCheckInterceptor())
+	r.Use(ValidateSignature())
+
+	r.POST("/slash", func(c *gin.Context) {
+		s, err := slack.SlashCommandParse(c.Request)
+		if err != nil {
+			log.Error(err)
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "Bad Request"})
+			return
+		}
+		switch s.Command {
+		case "/secret":
+			if err != nil {
+				log.Error(err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+			c.String(http.StatusOK, s.Text)
+		default:
+			return
+		}
+
 	})
 	port, err := strconv.ParseInt(os.Getenv("PORT"), 10, 64)
 	if err != nil {
