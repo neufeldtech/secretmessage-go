746751cbfc9f199cc3bb70640f910287b4d5c8ee
Jordan Neufeld <jordan@neufeldtech.com>
2023-05-06 18:47:59 +0000

    added devcontainer and sha property to /health

--- pkg/secretmessage/handle_health.go
+++ pkg/secretmessage/handle_health.go
@@ -2,24 +2,29 @@ package secretmessage
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/common/log"
 )
 
 func (ctl *PublicController) HandleHealth(c *gin.Context) {
+	version, ok := os.LookupEnv("NF_DEPLOYMENT_SHA")
+	if !ok {
+		version = "dev"
+	}
 	db, err := ctl.db.DB()
 	if err != nil {
 		log.Error(err)
-		c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN"})
+		c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN", "sha": version})
 		return
 	}
 
 	err = db.PingContext(c.Request.Context())
 	if err != nil {
 		log.Error(err)
-		c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN"})
+		c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN", "sha": version})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"status": "UP"})
+	c.JSON(http.StatusOK, gin.H{"status": "UP", "sha": version})
 }
