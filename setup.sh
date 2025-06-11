e5b8d295866e71fc2170fd76e3de4e601815ac87
Jordan Neufeld <jordan@neufeldtech.com>
2025-06-11 16:02:14 -0500

    fix callback settings locally

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 gomplate --context 'secrets=.devcontainer/secrets.json' -f .devcontainer/.env.tmpl -o .devcontainer/.env
+gomplate --context 'data=.devcontainer/secrets.json' -f manifest.yaml.tmpl -o manifest.yaml
 echo "GO RUN THIS: ngrok http --url=$(cat .devcontainer/secrets.json | jq .APP_URL) 8080"
