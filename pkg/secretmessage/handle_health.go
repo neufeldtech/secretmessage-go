947fa84bbc4daf4ee875382373f39c2e3075f7b0
Jordan Neufeld <neufeldtech@users.noreply.github.com>
2025-08-22 19:26:51 -0500

    Add db health checking back to /health. Allow POSTGRES_URI to override DATABASE_URL (#149)

--- pkg/secretmessage/handle_health.go
+++ pkg/secretmessage/handle_health.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func (ctl *PublicController) HandleHealth(c *gin.Context) {
@@ -12,21 +13,18 @@ func (ctl *PublicController) HandleHealth(c *gin.Context) {
 	if !ok {
 		version = "dev"
 	}
+	db, err := ctl.db.DB()
+	if err != nil {
+		ctl.logger.Error("error retrieving database connection", zap.Error(err))
+		c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN", "sha": version})
+		return
+	}
 
-	// Neon removed their free tier. It now costs me money to keep the database up, so let it sleep to save costs.
-
-	// db, err := ctl.db.DB()
-	// if err != nil {
-	// 	log.Error(err)
-	// 	c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN", "sha": version})
-	// 	return
-	// }
-
-	// err = db.PingContext(c.Request.Context())
-	// if err != nil {
-	// 	log.Error(err)
-	// 	c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN", "sha": version})
-	// 	return
-	// }
+	err = db.PingContext(c.Request.Context())
+	if err != nil {
+		ctl.logger.Error("error pinging database", zap.Error(err))
+		c.AbortWithStatusJSON(http.StatusServiceUnavailable, gin.H{"status": "DOWN", "sha": version})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"status": "UP", "sha": version})
 }
