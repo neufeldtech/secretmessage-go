16f7d6f1caa3bb3f16a22a12212f3893f8d5f0bb
Jordan Neufeld <neufeldtech@users.noreply.github.com>
2026-01-07 22:30:30 -0600

    migrate from md5+sha256 to argon2 (#169)
    * use argon2 for hashing
    
    use a fixed salt for hashing secretID's that we store in the database
    
    use a random salt for generating key for encryption, and store that in the database
    
    * test updates

--- pkg/secretmessage/crypto.go
+++ pkg/secretmessage/crypto.go
@@ -10,19 +10,48 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"regexp"
 	"unicode/utf8"
+
+	"golang.org/x/crypto/argon2"
 )
 
+// v2$<salt>$<ciphertext>
+// |    |        |
+// |    |        hex-encoded encrypted payload
+// |    |
+// |    salt rand.Text()
+// |
+// version identifier
+var V2EncryptionRegExp = regexp.MustCompile(`^v2\$([a-zA-Z0-9]+)\$([a-fA-F0-9]+)$`)
+
+const Pepper string = "XP47CYQ5SWBR2ZUFPUFROF55PR"
+
+// secureSecretID takes a string and returns a deterministic argon2 hash with a fixed system-wide salt
+// to be used for generating the hashed secretID we store in the database
+func secureSecretID(input string) string {
+	return hex.EncodeToString(argon2.IDKey([]byte(input), []byte(Pepper), 1, 64*1024, 1, 32))
+}
+
+// hash returns a sha256 hash of a given input in hex encoding
 func hash(s string) string {
 	hashBytes := sha256.Sum256([]byte(s))
 	return hex.EncodeToString(hashBytes[:])
 }
 
-func deriveCryptoKey(key string) []byte {
+// deriveCryptoKey takes a passphrase and returns a 16 byte slice which can be passed as an input to aes encrypt
+func deriveCryptoKey(passphrase string) []byte {
 	hasher := md5.New()
-	hasher.Write([]byte(key))
+	hasher.Write([]byte(passphrase))
 	return hasher.Sum(nil)
 }
+
+// deriveCryptoKeyV2 takes a passphrase and returns a 32 byte slice which can be used as an encryption key
+func deriveCryptoKeyV2(passphrase string, salt string) []byte {
+	return argon2.IDKey([]byte(passphrase), []byte(salt), 1, 64*1024, 1, 32)
+}
+
+// decrypt takes a hex-encoded input and passphrase and returns a decrypted string in plaintext
 func decrypt(input string, passphrase string) (string, error) {
 	var result string
 	if input == "" {
@@ -32,7 +61,20 @@ func decrypt(input string, passphrase string) (string, error) {
 		return result, fmt.Errorf("cannot decrypt with empty passphrase")
 	}
 
-	key := deriveCryptoKey(passphrase)
+	var key []byte
+
+	// If our encrypted string was prefaced with v2$ that means we encrypted with new v2 format
+	if V2EncryptionRegExp.MatchString(input) {
+		matches := V2EncryptionRegExp.FindStringSubmatch(input)
+		// extract the salt from first capture group
+		salt := matches[1]
+		// actual payload to decrypt (v2 prefix and salt removed)
+		input = matches[2]
+		key = deriveCryptoKeyV2(passphrase, salt)
+	} else {
+		key = deriveCryptoKey(passphrase)
+	}
+
 	ciphertext, err := hex.DecodeString(input)
 	if err != nil {
 		return result, err
@@ -59,11 +101,14 @@ func decrypt(input string, passphrase string) (string, error) {
 	return result, nil
 }
 
+// encrypt wraps encryptWithReader
 func encrypt(input string, passphrase string) (string, error) {
-	return encryptWithReader(rand.Reader, input, passphrase)
+
+	return encryptWithReader(rand.Reader, input, passphrase, rand.Text())
 }
 
-func encryptWithReader(rr io.Reader, input string, passphrase string) (string, error) {
+// encryptWithReader takes an input string and passphrase and returns a hex-encoded encrypted string
+func encryptWithReader(rr io.Reader, input string, passphrase string, salt string) (string, error) {
 	var result string
 	if input == "" {
 		return result, fmt.Errorf("cannot encrypt empty string")
@@ -71,7 +116,7 @@ func encryptWithReader(rr io.Reader, input string, passphrase string) (string, e
 	if passphrase == "" {
 		return result, fmt.Errorf("cannot encrypt with empty passphrase")
 	}
-	key := deriveCryptoKey(passphrase)
+	key := deriveCryptoKeyV2(passphrase, salt)
 	c, err := aes.NewCipher(key)
 	if err != nil {
 		return result, err
@@ -86,5 +131,5 @@ func encryptWithReader(rr io.Reader, input string, passphrase string) (string, e
 		return result, err
 	}
 	ciphertext := hex.EncodeToString(gcm.Seal(nonce, nonce, []byte(input), nil))
-	return ciphertext, nil
+	return fmt.Sprintf("v2$%s$%s", salt, ciphertext), nil
 }
--- pkg/secretmessage/crypto_test.go
+++ pkg/secretmessage/crypto_test.go
@@ -2,7 +2,6 @@ package secretmessage
 
 import (
 	"bytes"
-	"encoding/hex"
 	"io"
 	"reflect"
 	"testing"
@@ -34,11 +33,36 @@ func Test_hash(t *testing.T) {
 	}
 }
 
+func Test_secureSecretID(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "should return argon2 hash as hex",
+			args: args{
+				s: "my input string",
+			},
+			want: "268aa12ad29ed592c4e73e727fc2152bb5f1edf343c61a9f5bf00d0c14b0a572",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := secureSecretID(tt.args.s); got != tt.want {
+				t.Errorf("secureSecretID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func Test_deriveCryptoKey(t *testing.T) {
 	type args struct {
 		key string
 	}
-	w, _ := hex.DecodeString("8af774dc91d8be77e2baccfb8856c103")
 	tests := []struct {
 		name string
 		args args
@@ -49,13 +73,41 @@ func Test_deriveCryptoKey(t *testing.T) {
 			args: args{
 				key: "my input string",
 			},
-			want: w,
+			want: []byte{138, 247, 116, 220, 145, 216, 190, 119, 226, 186, 204, 251, 136, 86, 193, 3},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := deriveCryptoKey(tt.args.key); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("deriveCryptoKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func Test_deriveCryptoKeyV2(t *testing.T) {
+	type args struct {
+		key  string
+		salt string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "should return argon2 byte slice",
+			args: args{
+				key:  "my input string",
+				salt: "DZNUVLZNJVR3HOWSZPM2DEEKQ3",
+			},
+			want: []byte{86, 190, 230, 34, 101, 60, 153, 105, 175, 164, 186, 225, 142, 50, 228, 245, 103, 115, 222, 104, 57, 160, 91, 32, 64, 134, 165, 228, 139, 67, 11, 173},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deriveCryptoKeyV2(tt.args.key, tt.args.salt); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deriveCryptoKeyV2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -67,6 +119,7 @@ func Test_encryptWithReader(t *testing.T) {
 		rr         io.Reader
 		input      string
 		passphrase string
+		salt       string
 	}
 	tests := []struct {
 		name    string
@@ -75,18 +128,19 @@ func Test_encryptWithReader(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "successful encryption",
+			name: "successful encryption with salt",
 			args: args{
-				rr:         bytes.NewReader([]byte("0000000000000000")),
+				rr:         bytes.NewReader([]byte("00000000000000000000000000000000")),
 				input:      "the password is baseball123",
 				passphrase: "monkey",
+				salt:       "VC4TZT7JZOAAVFQ3F3N7GXF2RP",
 			},
-			want: "30303030303030303030303029c9922a9be75ba2e6be5afd32d19387baea51fa577c0c51dc9809a54adb9085490f109237d15a3262a585",
+			want: "v2$VC4TZT7JZOAAVFQ3F3N7GXF2RP$303030303030303030303030a091fd029ae527dfa9ed207ba09c537d8bdb5012f264f2a65d68a333fd58b378e5791a94d3060e919d4486",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := encryptWithReader(tt.args.rr, tt.args.input, tt.args.passphrase)
+			got, err := encryptWithReader(tt.args.rr, tt.args.input, tt.args.passphrase, tt.args.salt)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("encrypt() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -110,9 +164,17 @@ func Test_decrypt(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "successful decryption",
+			name: "successful decryption V1",
 			args: args{
 				input:      "30303030303030303030303029c9922a9be75ba2e6be5afd32d19387baea51fa577c0c51dc9809a54adb9085490f109237d15a3262a585",
+				passphrase: "monkey",
+			},
+			want: "the password is baseball123",
+		},
+		{
+			name: "successful decryption V2",
+			args: args{
+				input:      "v2$VC4TZT7JZOAAVFQ3F3N7GXF2RP$303030303030303030303030a091fd029ae527dfa9ed207ba09c537d8bdb5012f264f2a65d68a333fd58b378e5791a94d3060e919d4486",
 				passphrase: "monkey",
 			},
 			want: "the password is baseball123",
